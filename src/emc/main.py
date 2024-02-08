bc4e7e1a5bfd6b10cffad9c342775621deb41b17
Eertze van de Riet <eevdriet@gmail>
2024-02-08 09:10:16 +0100

    Fix for circular import Simulation

--- src/emc/main.py
+++ src/emc/main.py
@@ -4,6 +4,7 @@ from emc.data import DataLoader
 def main():
     loader = DataLoader('ascaris')
     scenarios = loader.load_scenarios()
+    df = scenarios[0].epidemiological_data
 
     print("succes")
 
