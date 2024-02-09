e891b7a373cdd519302da7cd605b58114443d41a
alexanderthn <alexanderneutel@gmail.com>
2024-02-09 15:27:01 +0100

    LSTM update

--- src/emc/classifiers/lstm.py
+++ src/emc/classifiers/lstm.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, Dropout
 import numpy as np
 
 from emc.classifiers import Classifier
@@ -17,8 +17,11 @@ class LSTMClassifier(Classifier):
         n_features = simulation_dim[1]
 
         self.model = Sequential()
-        self.model.add(LSTM(50, activation='relu', input_shape=(n_time_steps, n_features)))
-        self.model.add(Dense(1, activation='sigmoid'))
+        self.model.add(LSTM(30, activation='relu', return_sequences=True, input_shape=(n_time_steps, n_features)))
+        self.model.add(Dropout(0.2))
+        self.model.add(LSTM(20, activation='relu', return_sequences=True))
+        self.model.add(LSTM(10, activation='relu'))
+        self.model.add(Dense(1, activation='softmax'))
         self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
         self.model.fit(X_train, y_train, epochs=3, batch_size=32, verbose=1)
