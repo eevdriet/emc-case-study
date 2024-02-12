6126591b821f196a1fdfc1a6ba719402cf90aa88
MRDekeijzer <dekeijzer.matthijs@gmail.com>
2024-02-12 11:59:22 +0100

    Updated code: commented useless code

--- src/emc/classifiers/classifier.py
+++ src/emc/classifiers/classifier.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import pandas as pd
 
-from emc.model import Label
+from emc.model import Label, Scenario
 from emc.data import DataModel
 
 
@@ -16,7 +16,7 @@ class Classifier(ABC):
     def __init__(self):
         self.data: pd.DataFrame = pd.DataFrame()
 
-    def run(self, data: pd.DataFrame) -> float:
+    def run(self, data: list[Scenario]) -> float:
         """
         Run the classifier to find the labels of the given data
         :return: Results from the classifier
@@ -30,17 +30,61 @@ class Classifier(ABC):
         return self._test(X_test, y_test)
 
     @abstractmethod
-    def _preprocess(self, data: pd.DataFrame):
+    def _preprocess(self, data: list[Scenario]):
         """
         Preprocess the given data to
         - standardize data
         - remove columns that cannot be observed from epidemiological surveys
         - ...
         """
+
+        # features = pd.DataFrame()
+        # target = pd.DataFrame()
+
+        # time_data = data[0]._epi_data['time']
+        # n_host_data = data[0]._epi_data['n_host']
+        # n_host_eggpos_data = data[0]._epi_data['n_host_eggpos']
+        # a_epg_obs_data = data[0]._epi_data['a_epg_obs']
+        
+
+        # start_indices = []
+
+        # tolerance = 0.001
+
+        # start_indices.append(0)
+        # for i in range(1, len(time_data)):
+        #     if time_data[i] < tolerance and abs(time_data[i - 1]) > tolerance:
+        #         start_indices.append(i)
+
+        # time_result = [[] for _ in range(len(start_indices) * 4)]
+        # n_host_result = [[] for _ in range(len(start_indices) * 4)]
+        # n_host_eggpos_result = [[] for _ in range(len(start_indices) * 4)]
+        # a_epg_obs_result = [[] for _ in range(len(start_indices) * 4)]
+
+        # for i in range(len(start_indices)):
+        #     start_index = start_indices[i]
+        #     end_index = start_indices[i + 1] if i < len(start_indices) - 1 else len(time_data)
+        #     sublist_length = end_index - start_index
+            
+        #     for j in range(sublist_length):
+        #         sublist_index = i * 4 + (j % 4)
+        #         time_result[sublist_index].append(time_data[start_index + j])
+        #         n_host_result[sublist_index].append(n_host_data[start_index + j])
+        #         n_host_eggpos_result[sublist_index].append(n_host_eggpos_data[start_index + j])
+        #         a_epg_obs_result[sublist_index].append(a_epg_obs_data[start_index + j])
+
+
+        # print(time_result[0])
+
+        # target = [[] for _ in range(len(start_indices))]
+
+        
+
+
         # Only include relevant columns (that can be obtained from survey)
         ids = ['scen', 'sim']
         cols = ['time', 'n_host', 'n_host_eggpos', 'a_epg_obs']
-        print(data.columns)
+
         features = data[cols + ids]
 
         # Normalize relevant columns
--- src/emc/main.py
+++ src/emc/main.py
@@ -7,18 +7,16 @@ from collections import Counter
 def main():
     loader = DataLoader('ascaris')
     scenarios = loader.load_scenarios()
-    df = scenarios[0]._epi_data
 
     labelGenerator = LabelGenerator(scenarios)
     labelGenerator.generate()
 
     # Classifiers bouwen
     gb = GradientBoosting()
-    result = gb.run(scenarios[0]._epi_data)
+    result = gb.run(scenarios)
     print(Counter(result))
 
     print("succes")
-
 
 if __name__ == "__main__":
     main()
