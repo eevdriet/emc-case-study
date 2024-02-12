7eaa04f6de63ee488cde326115fd8d137f44b05b
Eertze van de Riet <eevdriet@gmail>
2024-02-12 11:43:37 +0100

    Update R script to output CSV files

--- scripts/load_monitor_age.R
+++ scripts/load_monitor_age.R
@@ -5,30 +5,23 @@ hookworm = readRDS("../data/timeline_db_main_hookworm.rds")
 n_simulations = 1000
 n_scenarios = 16 # scenarios 17-24 are not monogeneic
 
+relevant_cols = c("time", "age_cat", "n_host", "n_host_eggpos", "a_epg_obs")
+
 # Extract all survey information for each of the worms
 extract_surveys <- function(df) {
-    epi_survey <- data.frame()
-
+    name = deparse(substitute(df))
     for (scenario in 1:n_scenarios) {
         for (simulation in 1:n_simulations) {
             print(sprintf("Scenario %d:  %d", scenario, simulation))
-
-            df[[simulation]][[scenario]]$monitor_age['scen'] = scenario
-            df[[simulation]][[scenario]]$monitor_age['sim'] = simulation
-
-            epi_survey <- rbind(epi_survey, df[[simulation]][[scenario]]$monitor_age)
+            
+            df_inner = df[[simulation]][[scenario]]$monitor_age
+            epi_survey <- subset(df_inner, select=relevant_cols)
+            
+            file = sprintf("../csv/%s_monitor_ageSC%02dSIM%04d.csv", name, scenario, simulation)
+            write.csv(epi_survey, row.names = FALSE, file = file)
         }
     }
-
-    return(epi_survey)
 }
 
-ascaris_monitor_age = extract_surveys(ascaris)
-hookworm_monitor_age = extract_surveys(hookworm)
-
-# Export monitor age information
-monitor_age = ascaris_monitor_age
-save(monitor_age, file = "ascaris_monitor_age.rds")
-
-monitor_age = hookworm_monitor_age
-save(monitor_age, file = "hookworm_monitor_age.rds")
+extract_surveys(ascaris)
+extract_surveys(hookworm)
