615043e1f4ade7ca2bdeaec5fa33860a0e48ac94
Eertze van de Riet <eevdriet@gmail>
2024-02-17 15:20:23 +0100

    Moved example from main to LevelBuilder itself

--- src/emc/main.py
+++ src/emc/main.py
@@ -1,7 +1,8 @@
 from emc.data import DataLoader
-from emc.data.infection_tree import InfectionTree
+from emc.data.level_builder import LevelBuilder
 from emc.classifiers import GradientBoosting
 from emc.data import LabelGenerator
+from emc.model.scenario import Scenario
 from collections import Counter
 from emc.util import data_path
 
@@ -12,31 +13,10 @@ import numpy as np
 
 def main():
     worm = 'ascaris'
-    loader = DataLoader(worm, use_merged=False, load_efficacy=True)
+    loader = DataLoader(worm, use_merged=False, load_efficacy=False)
     df = loader.monitor_age
     scenarios = loader.load_scenarios()
 
-    # # NOTE: uncomment to generate new levels
-    # # Levels are currently saved in data, so you can instead retrieve them directly
-    # tree = InfectionTree(scenarios, df)
-
-    # path = data_path() / 'levels.txt'
-    # if not path.exists():
-    #     print("Levels does not exist, generate from InfectionTree")
-    #     return
-
-    # with open(path, 'r') as file:
-    #     level_simulations = eval(file.read())
-
-    # means, sds, mins, maxs = zip(*level_simulations[3])
-    # times = range(len(means))
-    # plt.errorbar(times, means, yerr=sds, color='b')
-    # plt.ylim(0, 1)
-    # plt.show()
-
-    # Classifiers bouwen
-    # gb = GradientBoosting()
-    # print(gb.run(scenarios))
 
 if __name__ == "__main__":
     main()
