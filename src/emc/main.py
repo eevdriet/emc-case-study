88bd78f059d5c075273533ff6441abe686b92862
MRDekeijzer <dekeijzer.matthijs@gmail.com>
2024-03-14 12:09:51 +0100

    Updated main to run correct code

--- src/emc/main.py
+++ src/emc/main.py
@@ -1,17 +1,10 @@
 # from emc.data import DataLoader
 from emc.data.constants import *
 from emc.data.data_loader import DataLoader
-
+import emc.data.policy_manager as policy_manager
 
 def main():
-    worm = Worm.ASCARIS
-    worm = worm.value
-    loader = DataLoader(worm)
-    df = loader.monitor_age
-
-    scenarios = loader.load_scenarios()
-    return 0
-
+    policy_manager.main()
 
 if __name__ == "__main__":
     main()
