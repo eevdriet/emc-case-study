700c08d5419d387c231ba74560c76e0977e0c11c
Matthijs <dekeijzer.matthijs@gmail.com>
2024-03-17 12:09:27 +0100

    Updated print statement

--- src/emc/data/policy_manager.py
+++ src/emc/data/policy_manager.py
@@ -176,9 +176,9 @@ class PolicyManager:
         results = {}
 
         for i in range(MC_EVALUATION_NUM):
+            logger.info(f"Policy costs ({float(score)}), Iteration {i + 1}/{MC_EVALUATION_NUM}")
             score = self.__calculate_score(policy)
             results[i] = score.as_dict()
-            logger.info(f"Policy costs ({float(score)}), Iteration {i + 1}/{MC_EVALUATION_NUM}")
 
         json_path = Paths.data('.') / "mc" / f"{self.worm}_{self.strategy}_{self.frequency}_{self.score_type.value}_{self.constructor.__name__}__{policy}.json"
         Writer.export_json_file(json_path, results)
